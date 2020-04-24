01b28f9a8478b735ceb06b9dfcb94558eb02d306
Isaac <isaacnbgomes@gmail.com>
2020-04-24 09:36:06 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -11,7 +11,7 @@ export default [
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non lobortis massa, porttitor luctus risus. Mauris vel justo erat. Nullam sed eleifend augue. Aliquam in libero lacinia, sodales nunc sit amet, malesuada felis.",
         cover: "Capa RW 960x540 (sem fundo).png",
         file: "SAEST E VOCÊ - EDIÇÃO 02 - PROGRAMA PERMANENCIA - 26.04.18.mp3"
-    },
+    },  
     {
         title: "Edição 03: Estudante Saudável",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non lobortis massa, porttitor luctus risus. Mauris vel justo erat. Nullam sed eleifend augue. Aliquam in libero lacinia, sodales nunc sit amet, malesuada felis.",
@@ -31,25 +31,25 @@ export default [
         file: "SAEST E VOCÊ - EDIÇÃO 05 - MORADIA ESTUDANTIL - 14.06.2018.mp3"
     },
     {
-        title: "Edição 06: Promoção à Saúde Mental e Qualidade de Vida dos Estudantes da UFPA",
+        title: "Edição 06: Programa à Saúde Mental e Qualidade de Vida dos Estudantes da UFPA",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non lobortis massa, porttitor luctus risus. Mauris vel justo erat. Nullam sed eleifend augue. Aliquam in libero lacinia, sodales nunc sit amet, malesuada felis.",
         cover: "Capa RW 960x540 (sem fundo).png",
         file: "SAEST E VOCÊ - EDIÇÃO 06 - PROQVIDA - 30.08.2018.mp3"
     },
     {
-        title: "Edição 07: Visitas Domiciliares",
+        title: "Edição 07: Visitas Domicialiares",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non lobortis massa, porttitor luctus risus. Mauris vel justo erat. Nullam sed eleifend augue. Aliquam in libero lacinia, sodales nunc sit amet, malesuada felis.",
         cover: "Capa RW 960x540 (sem fundo).png",
         file: "SAEST E VOCÊ - EDIÇÃO 07 - VISITAS DOMICILIARES - 25.10.2018.mp3"
     },
     {
-        title: "Edição 08: Ações Para Alunos Surdos",
+        title: "Edição 08: Ações para alunos surdos",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non lobortis massa, porttitor luctus risus. Mauris vel justo erat. Nullam sed eleifend augue. Aliquam in libero lacinia, sodales nunc sit amet, malesuada felis.",
         cover: "Capa RW 960x540 (sem fundo).png",
         file: "SAEST E VOCÊ - EDIÇÃO 08 - AÇÕES PARA ALUNOS SURDOS - 08.11.18.mp3"
     },
     {
-        title: "Edição 09: Ações Para Alunos Autistas",
+        title: "Edição 09: Ações para alunos autistas",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non lobortis massa, porttitor luctus risus. Mauris vel justo erat. Nullam sed eleifend augue. Aliquam in libero lacinia, sodales nunc sit amet, malesuada felis.",
         cover: "Capa RW 960x540 (sem fundo).png",
         file: "SAEST E VOCÊ - EDIÇÃO 09 - AÇÕES PARA ALUNOS AUTISTAS - 06.12.2018.mp3"
@@ -61,7 +61,7 @@ export default [
         file: "SAEST E VOCÊ - EDIÇÃO 10 - ACOMPANHAMENTO PEDAGOGICO - 20.12.18.mp3"
     },
     {
-        title: "Edição 11: Ações Para Pessoas com Deficiência Física na UFPA",
+        title: "Edição 11: Ações para pessoas com deficiência física na UFPA",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non lobortis massa, porttitor luctus risus. Mauris vel justo erat. Nullam sed eleifend augue. Aliquam in libero lacinia, sodales nunc sit amet, malesuada felis.",
         cover: "Capa RW 960x540 (sem fundo).png",
         file: "SAEST E VOCÊ - EDIÇÃO 11 - AÇÕES PARA PESSOAS COM DEFICIENCIA - 04.04.2019.mp3"
@@ -79,7 +79,7 @@ export default [
         file: "SAEST E VOCÊ - EDIÇÃO 13 - RESTAURANTES UNIVERISTÁRIOS - 04.07.2019.mp3"
     },
     {
-        title: "Edição 14: Inclusão, Acessibilidade e Diversidade",
+        title: "Edição 14: Inclusão, acessibilidade e diversidade",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non lobortis massa, porttitor luctus risus. Mauris vel justo erat. Nullam sed eleifend augue. Aliquam in libero lacinia, sodales nunc sit amet, malesuada felis.",
         cover: "Capa RW 960x540 (sem fundo).png",
         file: "SAEST E VOCÊ - EDIÇÃO 14 - INCLUSÃO, ACESSIBILIDADE E DIVERSIDADE - 11.07.2019.mp3"
@@ -97,9 +97,9 @@ export default [
         file: "SAEST E VOCÊ - EDIÇÃO 16 - SAÚDE MENTAL NA UNIVERSIDADE - 03.10.2019.mp3"
     },
     {
-        title: "Edição 17: Ações Para Alunos com Deficiência Visual",
+        title: "Edição 17: Ações para os alunos com deficiência visual",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non lobortis massa, porttitor luctus risus. Mauris vel justo erat. Nullam sed eleifend augue. Aliquam in libero lacinia, sodales nunc sit amet, malesuada felis.",
         cover: "Capa RW 960x540 (sem fundo).png",
-        file: "SAEST E VOCÊ - EDIÇÃO 17 - AÇÕES PARA ALUNOS COM DEFICIÊNCIA VISUAL.mp3"
+        file: "SAEST E VOCÊ - EDIÇÃO 17 - AÇÕES PARA ALUNOS COM DEFICIÊNCIA VISUAL - 05.12.2019.mp3"
     }
 ];
